2491b78717f081115662f0238bdb8ead497c00c5
DELL-7240\THAI <nguoicuatheki20@gmail.com>
2023-05-24 12:27:04 +0700

    add template admin

---
+++ src/_mock/user.js
@@ -0,0 +1,30 @@
+import { faker } from '@faker-js/faker';
+import { sample } from 'lodash';
+
+// ----------------------------------------------------------------------
+
+const users = [...Array(24)].map((_, index) => ({
+  id: faker.datatype.uuid(),
+  avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
+  name: faker.name.fullName(),
+  // company: faker.company.name(),
+  email: faker.internet.email(),
+  isVerified: faker.datatype.boolean(),
+  wallet: faker.datatype.number({ max: 1000 }),
+  status: sample(['active', 'banned']),
+  // role: sample([
+  //   'Leader',
+  //   'Hr Manager',
+  //   'UI Designer',
+  //   'UX Designer',
+  //   'UI/UX Designer',
+  //   'Project Manager',
+  //   'Backend Developer',
+  //   'Full Stack Designer',
+  //   'Front End Developer',
+  //   'Full Stack Developer',
+  // ]),
+  phone: faker.phone.number('09########')
+}));
+
+export default users;
